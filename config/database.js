28c02b986ce4f363b11f78200dd2ce83aaed4428
Flo <Floren@Florencios-iMac.local>
2020-04-09 22:05:03 -0500

    MongoDB Connect

--- config/database.js
+++ config/database.js
@@ -7,7 +7,6 @@ mongoose.connect(_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 });
 */
 
-
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://floren:Privat333@cluster0-ugmu0.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,3 +15,29 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+
+/*
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'cluster0-shard-00-01-ugmu0.mongodb.net:27017 ';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+*/
+
