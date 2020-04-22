544c858e2d55c7f7a75788edfaee346009e324c1
Flo <Floren@Florencios-iMac.local>
2020-04-22 17:02:47 -0500

    test

--- FinalCurso/src/app/hero/hero.component.ts
+++ FinalCurso/src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -10,6 +10,28 @@ export class HeroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+}
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
   }
 
 }
